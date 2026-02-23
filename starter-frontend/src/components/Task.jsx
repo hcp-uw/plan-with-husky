4322ce3848e4e3e36ad32619f28e9893862df61b
chuang0120 <chuang43@uw.edu>
2026-02-23 14:02:11 -0800

    still not working

--- starter-frontend/src/components/Task.jsx
+++ starter-frontend/src/components/Task.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import "./Task.css";
 
-const Task = () => {
+// A singular task.
+const Task = (props) => {
+  const [desc, setDesc] = useState(props.desc);
+  const [date, setDate] = useState(props.date);
+  const [categ, setCateg] = useState(props.categ);
+  const [points, setPoints] = useState(props.points);
+
   return (
-    <div>
-      <p>Task</p>
-    </div>
+    <>
+      <div className="container">
+        <div className="check">
+          <input
+            type="checkbox"
+            name="option_name"
+            value="option_value"
+          ></input>
+        </div>
+        <div>{desc}</div>
+        <div className="right">{categ}</div>
+        <div>{date}</div>
+        <div>{points}</div>
+      </div>
+    </>
   );
 };
 
