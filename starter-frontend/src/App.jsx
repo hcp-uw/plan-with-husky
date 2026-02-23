351feae3179cfdee3e594be4cac52cae37d08d68
chuang0120 <chuang43@uw.edu>
2026-02-23 12:15:04 -0800

    rerouted home screen to include multiple pages

--- starter-frontend/src/App.jsx
+++ starter-frontend/src/App.jsx
@@ -1,47 +1,12 @@
 import { useState } from "react";
-import "./App.css";
-import MyHusky from "./components/MyHusky";
-import Task from "./components/Task";
+import "./styles/App.css";
+import Home from "./pages/Home";
 
-function App() {
-  let name = "John Doe";
-
+export default function App() {
   return (
     <>
-      <div class="parent">
-        <div class="navigation">
-          <div>Plan With Husky</div>
-          <div class="buttons">
-            <button>Home</button>
-            <button>Notifications</button>
-            <button>Settings</button>
-            <button id="profile-button">Your Profile</button>
-          </div>
-        </div>
-        <div class="title">
-          <h1>Welcome back, {name}!</h1>
-        </div>
-        <div class="menu">
-          <div>todo</div>
-          <div>calendar</div>
-          <div>my husky</div>
-          <div>shop</div>
-        </div>
-        <div class="husky">
-          <MyHusky />
-        </div>
-        <div class="content">
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-        </div>
-      </div>
-      <div class="footer">footer</div>
+      <Home />
     </>
   );
 }
 
-export default App;
---
+++ starter-frontend/src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import "./Home.css";
+import MyHusky from "../components/MyHusky";
+import Task from "../components/Task";
+
+export default function Home() {
+    let name = "John Doe";
+
+  return (
+    <>
+      <div class="parent">
+        <div class="navigation">
+          <div>Plan With Husky</div>
+          <div class="buttons">
+            <button>Home</button>
+            <button>Notifications</button>
+            <button>Settings</button>
+            <button id="profile-button">Your Profile</button>
+          </div>
+        </div>
+        <div class="title">
+          <h1>Welcome back, {name}!</h1>
+        </div>
+        <div class="menu">
+          <div>todo</div>
+          <div>calendar</div>
+          <div>my husky</div>
+          <div>shop</div>
+        </div>
+        <div class="husky">
+          <MyHusky />
+        </div>
+        <div class="content">
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+        </div>
+      </div>
+      <div class="footer">footer</div>
+    </>
+  );
+}
