415ecc45fe80bc3c0c331674a35960fe7c46c4c2
chuang0120 <chuang43@uw.edu>
2026-02-23 13:36:02 -0800

    everything routed, change between pages with /home and /login

--- starter-frontend/src/App.jsx
+++ starter-frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+
 import "./styles/App.css";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <>
-      <Login />
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
