718db7459d664efac3b75cabde7df689ca1898dd
chuang0120 <chuang43@uw.edu>
2026-03-04 14:38:51 -0800

    basic calendar page with sideMenu navigation included

--- starter-frontend/src/pages/Calendar/Calendar.jsx
+++ starter-frontend/src/pages/Calendar/Calendar.jsx
@@ -1,6 +1,12 @@
+import "../Home/Home.css";
+import SideBar from "../../components/SideBar/SideBar";
+
 export default function Calendar () {
     return (
         <>
+            <div className="menu">
+                <SideBar />
+            </div>
             <p>Yippe!! Here is calendar text.</p>
         </>
     );
