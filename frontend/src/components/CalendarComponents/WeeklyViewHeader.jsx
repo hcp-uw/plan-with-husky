d09cf51327d9662abaa2e0b0a5253784e08bc849
chuang0120 <chuang43@uw.edu>
2026-05-17 23:17:34 -0700

    finished designs for task and event screens

--- frontend/src/components/CalendarComponents/WeeklyViewHeader.jsx
+++ frontend/src/components/CalendarComponents/WeeklyViewHeader.jsx
@@ -9,32 +9,46 @@ export default function WeeklyViewHeader({ onAddClick, currentWeek, currentDate,
     return (
         <div>
             <div className="topRow"> 
-                <p className="headerDate">Today's date: {today.toLocaleDateString()}, {weekday}</p>
-                <button className="addEventButton" onClick={onAddClick}>+ New Event</button>
-                <button onClick={view === "week" ? onPrevWeek : onPrevMonth} className="arrow-button">
-                    ←
-                </button>
-                <p>
-                    {view === "week" 
-                        ? currentWeek.toLocaleDateString([], {
-                            month: "long",
-                            year: "numeric"
-                        })
-                        : currentDate.toLocaleDateString([], {
-                            month: "long",
-                            year: "numeric"
-                        })
-                    
-                    }
-                    
-                </p>
-                <button onClick={view === "week" ? onNextWeek : onNextMonth} className="arrow-button">
-                    →
-                </button>
-                <button onClick={() => setView(view === "week" ? "month" : "week")} className="view-button">
-                    {view === "week" ? "Monthly view" : "Weekly view"}
-                </button>
+                <h1 className="calendar-title">
+                    Calendar
+                </h1>
+
+                <div className="headerButtons">
+                    <button className="addEventButton" onClick={onAddClick}>+ New Event</button>
+                    <button onClick={view === "week" ? onPrevWeek : onPrevMonth} className="arrow-button">
+                        ←
+                    </button>
+                    <p className="monthInArrows">
+                        {view === "week" 
+                            ? currentWeek.toLocaleDateString([], {
+                                month: "long",
+                                year: "numeric"
+                            })
+                            : currentDate.toLocaleDateString([], {
+                                month: "long",
+                                year: "numeric"
+                            })
+                        
+                        }
+                        
+                    </p>
+                    <button onClick={view === "week" ? onNextWeek : onNextMonth} className="arrow-button">
+                        →
+                    </button>
+                    <button onClick={() => setView(view === "week" ? "month" : "week")} className="view-button">
+                        {view === "week" ? "Monthly view" : "Weekly view"}
+                    </button>
+                </div>  
             </div>
+            <p className="headerDate">
+                {today.toLocaleTimeString("en-US", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric"
+                })
+                    
+                }
+            </p>
             <div className="week">
                 <div className="timeSpacer" />
 
